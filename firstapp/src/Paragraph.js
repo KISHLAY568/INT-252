bb218cd9452a97ebea5b589709268471ca37fab3
KISHLAY568 <kishlayk568@gmail.com>
2024-08-30 11:16:23 +0530

    first commit

--- firstapp/src/Paragraph.js
+++ firstapp/src/Paragraph.js
@@ -3,4 +3,12 @@ function Paragraph() {
   return <p>Kishlay</p>;
 }
 
+function Secondpara() {
+  return <p>This is paragraph 2</p>;
+}
+function Thirdpara() {
+  return <p>This is paragraph 3</p>;
+}
+
 export default Paragraph;
+export { Secondpara, Thirdpara };
