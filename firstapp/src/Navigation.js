47c96bfe42a3b4343159f8ba511c6bf911aa8173
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 16:15:14 +0530

    first commit

--- firstapp/src/Navigation.js
+++ firstapp/src/Navigation.js
@@ -1,16 +1,16 @@
 import React from "react";
-
-function Navigation() {
+import PropTypes from "prop-types";
+function Navigation(props) {
   return (
     <nav class="nav">
       <span>
-        <a href="#home">Home</a>
+        <a href="#home">{props.title}</a>
       </span>
       <span>
         <a href="#about">About Us</a>
       </span>
       <span>
-        <a href="#contact">Contact Us</a>
+        <a href="#contact">{props.type}</a>
       </span>
       <span>
         <a href="#service">Services</a>
