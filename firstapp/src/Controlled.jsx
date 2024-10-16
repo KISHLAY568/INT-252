c8f18e0861db09a8e8c3f56a39b47ba43a5c4073
KISHLAY568 <kishlayk568@gmail.com>
2024-10-16 16:07:01 +0530

    react

--- firstapp/src/Controlled.jsx
+++ firstapp/src/Controlled.jsx
@@ -1,11 +1,13 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 function Controlled() {
   const [inputValue, setInputValue] = useState("");
   const handleChange = (e) => {
     setInputValue(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputValue);
-  }
+  };
+}
+export default Controlled;
