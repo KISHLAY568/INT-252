64c1e9d258462bcb7d0e8507e39b452f811d6e5d
KISHLAY568 <kishlayk568@gmail.com>
2024-10-18 10:58:07 +0530

    react

--- firstapp/src/Formvalidation.jsx
+++ firstapp/src/Formvalidation.jsx
@@ -36,5 +36,59 @@ const Formvalidation = () => {
       errors.confirmPassword = "Password and Confirm Password should be same";
     }
     setFormError(errors);
+
+    if(Object.keys(errors).length === 0){
+      alert("Form Submitted");
+    }
   };
+
+  return (
+    <div className="login-form-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          {formError.username && <span>{formError.username}</span>}
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {formError.email && <span>{formError.email}</span>}
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {formError.password && <span>{formError.password}</span>}
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {formError.confirmPassword && <span>{formError.confirmPassword}</span>}
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
+
+export default Formvalidation;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -21,6 +21,7 @@ import Hook from "./Activity5.jsx";
 import Useref from "./Regi.jsx";
 import Usereducer from "./Usereducer.jsx";
 import LoginForm from "./form.jsx";
+import Formvalidation from "./Formvalidation.jsx";
 import Addition from "./acticity8.jsx";
 import Timer from "./Timer.jsx";
 import { Add, Subtract, Divide, Multiply } from "./calculator.jsx";
@@ -63,7 +64,7 @@ ReactDOM.render(
     </ol> */}
     {/* <Img /> */}
     {/* <Hook/> */}
-    <LoginForm />
+    <Formvalidation />
   </>,
   document.getElementById("root")
 );
