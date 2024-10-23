509f3b987dfde9f44ef7edaf382055a33aab38f0
KISHLAY568 <kishlayk568@gmail.com>
2024-10-23 16:40:09 +0530

    react

--- firstapp/src/Get.jsx
+++ firstapp/src/Get.jsx
@@ -19,6 +19,27 @@ const Get = () => {
         setIsLoading(false);
       });
   }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Get;
